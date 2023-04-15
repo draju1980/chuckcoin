2b00d8ef229e8ee5557843e1c5b5d7ba5c6f411b
draju1980 <draju@outlook.com>
2023-04-15 20:02:54 +0100

    added

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"goblockchain/utils"
+)
+
+func main() {
+	//fmt.Println(utils.IsFoundHost("127.0.0.1", 3000))
+	//fmt.Println(utils.FindNeighbors("127.0.0.1", 3000, 0, 3, 3000, 3003))
+	fmt.Println(utils.GetHost())
+
+}
